2f4fb4288f9dff108d5b65755edba187904decc7
Max <ageev.maks2014@gmail.com>
2019-02-17 01:34:13 +0500

    11-babel-options-polyfill

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -1,6 +1,8 @@
 'use strict';
 const $ = require('jquery');
 require('bootstrap');
+// include this on every page to make sure Promise exists
+require('babel-polyfill');
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
