0073677bf8ef0bfd06a09eb96d03371564b7bc10
Max <ageev.maks2014@gmail.com>
2019-02-17 12:52:36 +0500

    14-bootstrap-fonts

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -3,7 +3,7 @@ const $ = require('jquery');
 require('bootstrap');
 // include this on every page to make sure Promise exists
 require('babel-polyfill');
-
+require('bootstrap/dist/css/bootstrap.css');
 require('../css/main.css');
 
 $(document).ready(function() {
