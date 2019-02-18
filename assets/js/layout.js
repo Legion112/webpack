1b765097c7a66385a3cfface6d23bdc3fa19d95b
Max <ageev.maks2014@gmail.com>
2019-02-18 13:59:03 +0500

    18-bootstrap-sass

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,11 +1,9 @@
 'use strict';
 const $ = require('jquery');
-require('bootstrap');
+require('bootstrap-sass');
 // include this on every page to make sure Promise exists
 require('babel-polyfill');
-require('bootstrap/dist/css/bootstrap.css');
-require('font-awesome/css/font-awesome.css');
-require('../css/main.css');
+require('../css/main.scss');
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
