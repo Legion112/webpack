bf6b5f6843eb017847aa9a0a874c2eccc7b22a8c
Max <ageev.maks2014@gmail.com>
2019-02-18 18:11:44 +0500

    25-es6-import-export

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,9 +1,9 @@
 'use strict';
-const $ = require('jquery');
-require('bootstrap-sass');
+import $ from 'jquery';
+import'bootstrap-sass';
 // include this on every page to make sure Promise exists
-require('babel-polyfill');
-require('../css/main.scss');
+import'babel-polyfill';
+import'../css/main.scss';
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
